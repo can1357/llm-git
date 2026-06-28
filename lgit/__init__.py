f2f20328bc75eb98eba4427c0288d16dee39f663
can1357 <me@can.ac>
2026-06-28 17:12:14 +0200

    release: bumped version to 4.2.0

--- lgit/__init__.py
+++ lgit/__init__.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING, Any
 try:
     __version__ = metadata.version("lgit-cli")
 except metadata.PackageNotFoundError:
-    __version__ = "4.1.0"
+    __version__ = "4.2.0"
 
 _CORE_MODEL_EXPORTS = (
     "Mode",
